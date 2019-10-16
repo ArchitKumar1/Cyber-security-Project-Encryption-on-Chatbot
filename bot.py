13977dca298d4df5e53f390ed6122e4befba48ab
Archit_Kumar1 <architkumar85@gmail.com>
2019-10-17 04:43:13 +0530

    new

---
+++ bot.py
@@ -0,0 +1,77 @@
+import nltk
+import warnings
+warnings.filterwarnings("ignore")
+import csv
+import numpy as np
+import random
+import string
+
+
+f=open('Hotelbot.txt','r',errors = 'ignore')
+raw=f.read()
+raw=raw.lower()# converts to lowercase
+
+sent_tokens = nltk.sent_tokenize(raw)# converts to list of sentences 
+word_tokens = nltk.word_tokenize(raw)# converts to list of words
+
+
+sent_tokens[:2]
+word_tokens[:5]
+
+lemmer = nltk.stem.WordNetLemmatizer()
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
+GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
+
+def greeting(sentence):
+    for word in sentence.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES)
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+# Generating response
+def response(user_response):
+    robo_response=''
+    sent_tokens.append(user_response)
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx=vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if(req_tfidf==0):
+        robo_response=robo_response+"I am sorry! I don't understand you"
+        return robo_response
+    else:
+        robo_response = robo_response+sent_tokens[idx]
+        return robo_response
+
+def solve(user_response):
+    if(user_response!='bye'):
+        if(user_response=='thanks' or user_response=='thank you' ):
+            flag=False
+            return("Admin: You are welcome..")
+        else:
+            if(greeting(user_response)!=None):
+                return("Admin: "+greeting(user_response))
+            else:
+                user_response1=response(user_response)
+                sent_tokens.remove(user_response)
+                return(user_response1)
+                
+    else:
+        flag=False
+        return("Admin: Bye! take care..")
+
+
--- main.py
+++ main.py
@@ -1,42 +1,21 @@
-from flask import Flask,render_template,url_for,request
+from flask import Flask, render_template, url_for, request
 import sqlite3
+from bot import solve
+
 app = Flask(__name__)
-
-@app.route('/enternew')
-def new_student():
-   return render_template('student.html')
-
-@app.route('/addrec',methods = ['POST', 'GET'])
-def addrec():
-   if request.method == 'POST':
-      try:
-         nm = request.form['nm']
-         addr = request.form['add']      
-         with sqlite3.connect("database.db") as con:
-            cur = con.cursor()
-            cur.execute("INSERT INTO students (name,addr) VALUES (?,?)",(nm,addr) )
-            con.commit()
-            msg = "Record successfully added"
-      except:
-         con.rollback()
-         msg = "error in insert operation"
-      finally:
-         return render_template("result.html",msg = msg)
-         con.close()
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('cb.html')
 
-@app.route('/list')
-def list():
-   con = sqlite3.connect("database.db")
-   con.row_factory = sqlite3.Row
-   
-   cur = con.cursor()
-   cur.execute("select * from students")
-   
-   rows = cur.fetchall(); 
-   return render_template("list.html",rows = rows)
+
+@app.route('/addrec', methods=['POST', 'GET'])
+def addrec():
+    if request.method == 'POST':
+        nm = request.form["msg"]
+
+        return render_template('cb.html',res = solve(nm))
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
