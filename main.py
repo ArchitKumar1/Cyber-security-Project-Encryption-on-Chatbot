b3b5c31b28703ab6fa66fd612df996c6ca1bfae3
Archit_Kumar1 <architkumar85@gmail.com>
2019-10-18 02:50:37 +0530

    new

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, request
 import sqlite3
 from bot import solve
+import math
 
 app = Flask(__name__)
 
@@ -13,8 +14,25 @@ def home():
 def addrec():
     if request.method == 'POST':
         nm = request.form["msg"]
+        E = int(nm)
+        #decrypt
+        d = 13
+        n = 143
+        
 
-        return render_template('cb.html',res = solve(nm))
+        C = pow(E,d,n)
+        C = E
+        input = ""
+        while(C>0):
+        	rem = C%100
+        	c = chr(rem)
+        	input+= c
+        	C = C//100
+        
+        input = input[::-1]
+        output = solve(input)
+        
+        return render_template('cb.html',res = output)
 
 
 if __name__ == '__main__':
